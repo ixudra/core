67bc417c598d82d8a672bc56b14d6af5bf4f867c
Jan Oris <jan.oris@gmail.com>
2015-03-20 23:02:07 +0100

    Updated BaseRequest to enable boolean validation

--- src/Ixudra/Core/Http/Requests/BaseRequest.php
+++ src/Ixudra/Core/Http/Requests/BaseRequest.php
@@ -5,9 +5,24 @@ use Illuminate\Foundation\Http\FormRequest;
 
 abstract class BaseRequest extends FormRequest {
 
+    public function all()
+    {
+        return array_replace_recursive($this->getInput(), $this->files->all());
+    }
+
     public function getInput()
     {
         return $this->input();
+    }
+
+    protected function convertToTruthyValue($input, $key)
+    {
+        $value = false;
+        if( array_key_exists($key, $input) && $input[ $key ] != '' ) {
+            $value = true;
+        }
+
+        return $value;
     }
 
 }
